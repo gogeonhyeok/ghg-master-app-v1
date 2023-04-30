ae0783a2bba5be6c3df8fdad7506c484a091879f
gogeonhyeok <gogeonhyeok@outlook.com>
2023-04-30 23:25:24 +0800

    commit

--- components/farm/list-farm.js
+++ components/farm/list-farm.js
@@ -1,8 +1,9 @@
-export default function ListFarm({ farms }) {
+export default function ListFarm({ items }) {
+  console.log(items);
   return (
     <>
       <ul>
-        {farms.map((entry) => (
+        {items.map((entry) => (
           <li>
             <h2>{entry.name}</h2>
             <p>{entry.description}</p>
@@ -12,39 +13,4 @@ export default function ListFarm({ farms }) {
       </ul>
     </>
   );
-}
-
-export async function getServerSideProps() {
-  try {
-    const response = await fetch(
-      'https://data.mongodb-api.com/app/data-efhph/endpoint/data/v1/action/find',
-      {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'api-key':
-            'EkYheuKC7iqVZ1H2FejbfNLjSj9RHDw26HWMm8Z31Cpo3nJlK6Q8jAa7gUpX4Ow7',
-        },
-        body: JSON.stringify({
-          dataSource: 'Cluster0',
-          database: 'ghg-master-api-v1',
-          collection: 'farms',
-          filter: {},
-          sort: {
-            completedAt: 1,
-          },
-        }),
-      }
-    );
-    let responseBody = await response.json();
-
-    return {
-      props: {
-        farms: responseBody.documents,
-      },
-    };
-  } catch (e) {
-    console.error(e);
-  }
 }
--- pages/farm/index.js
+++ pages/farm/index.js
@@ -1,11 +1,47 @@
 import ListFarm from '../../components/farm/list-farm';
+import Link from 'next/link';
 
-export default function Farm() {
+export default function Farm({ farms }) {
   return (
     <>
       <Link href="/farm/create">Create</Link>
       <Link href="/">Home</Link>
-      <ListFarm />
+      <ListFarm items={farms} />
     </>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const response = await fetch(
+      'https://data.mongodb-api.com/app/data-efhph/endpoint/data/v1/action/find',
+      {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key':
+            'EkYheuKC7iqVZ1H2FejbfNLjSj9RHDw26HWMm8Z31Cpo3nJlK6Q8jAa7gUpX4Ow7',
+        },
+        body: JSON.stringify({
+          dataSource: 'Cluster0',
+          database: 'ghg-master-api-v1',
+          collection: 'farms',
+          filter: {},
+          sort: {
+            completedAt: 1,
+          },
+        }),
+      }
+    );
+    let responseBody = await response.json();
+
+    return {
+      props: {
+        farms: responseBody.documents,
+      },
+    };
+  } catch (e) {
+    console.error(e);
+  }
 }
