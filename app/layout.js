90dcfe098d2bf3ddfdb2652b53790186e221859b
gogeonhyeok <gogeonhyeok@outlook.com>
2023-07-10 00:39:59 +0800

    sidebar designing

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
         }}>
           <SideBar />
           <div>
-            {children}
+          {children}
           </div>
         </div>
       </body>
