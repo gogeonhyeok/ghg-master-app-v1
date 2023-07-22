132ebf9607153c13f68c3ab7a214f3cbd7d01586
gogeonhyeok <gogeonhyeok@outlook.com>
2023-07-22 21:10:49 +0800

    Improve sidebar

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children }) {
           gap: 4
         }}>
           <SideBar />
-          <div>
+          <div style={{marginLeft: 300}}>
           {children}
           </div>
         </div>
